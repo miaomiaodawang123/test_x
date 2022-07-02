a9b666bd40807d8415c0b1fb32422b606bf617af
miaomiaodawang123 <linxinyu1998123@163.com>
2022-07-02 15:13:27 +0800

    add:第一次使用 git

---
+++ newTest/src/main/java/od/NewTest.java
@@ -0,0 +1,12 @@
+package od;
+
+public class NewTest {
+    /**
+     * auth : duke
+     * time : 2022/7/2
+     * dec : the first use github by idea
+     */
+    void say(){
+        System.out.println("第一次使用 github ....");
+    }
+}
